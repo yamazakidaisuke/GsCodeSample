05e291ee942abf0df0acb2a5878cc157f81329a0
Daisu_yamazaki <yamazaki_daisuke@hotmail.co.jp>
2022-09-22 16:40:10 +0900

    Add files via upload

---
+++ TailwindCSS_example/lesson03/tail.blade.php
@@ -0,0 +1,86 @@
+<!-- resources/views/books.blade.php -->
+<x-app-layout>
+
+    <!--ヘッダー[START]-->
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <form action="{{ route('book_index') }}" method="GET" class="w-full max-w-lg">
+                <x-button class="bg-gray-100 text-gray-900">{{ __('Dashboard') }}</x-button>
+            </form>
+        </h2>
+    </x-slot>
+    <!--ヘッダー[END]-->
+            
+        <!-- バリデーションエラーの表示に使用-->
+       <x-errors id="errors" class="bg-blue-500 rounded-lg">{{$errors}}</x-button>
+        <!-- バリデーションエラーの表示に使用-->
+    
+    <!--全エリア[START]-->
+    <div class="flex bg-gray-100">
+
+        <!--左エリア[START]--> 
+        <div class="text-gray-700 text-left px-4 py-4 m-2">
+            
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-500 font-bold">
+                    本を管理する
+                </div>
+            </div>
+
+
+            <!-- 本のタイトル -->
+            <form action="{{ url('books') }}" method="POST" class="w-full max-w-lg">
+                @csrf
+                  <div class="flex flex-col px-2 py-2">
+                   <!-- カラム１ -->
+                    <div class="w-full md:w-1/1 px-3 mb-2 md:mb-0">
+                      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                       Book Name
+                      </label>
+                      <input name="item_name" class="appearance-none block w-full text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="">
+                    </div>
+                    <!-- カラム２ -->
+                    <div class="w-full md:w-1/1 px-3">
+                      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        金額
+                      </label>
+                      <input name="item_amount" class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="">
+                    </div>
+                    <!-- カラム３ -->
+                    <div class="w-full md:w-1/1 px-3 mb-2 md:mb-0">
+                      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        数
+                      </label>
+                      <input name="item_number" class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="">
+                    </div>
+                    <!-- カラム４ -->
+                    <div class="w-full md:w-1/1 px-3 mb-6 md:mb-0">
+                      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        発売日
+                      </label>
+                      <input name="published" type="date" class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="">
+                    </div>
+                  </div>
+                  <!-- カラム５ -->
+                  <div class="flex flex-col">
+                      <div class="text-gray-700 text-center px-4 py-2 m-2">
+                             <x-button class="bg-blue-500 rounded-lg">送信</x-button>
+                      </div>
+                   </div>
+            </form>
+        </div>
+        <!--左エリア[END]--> 
+    
+    
+    <!--右側エリア[START]-->
+    <div class="flex-1 text-gray-700 text-left bg-blue-100 px-4 py-2 m-2">
+
+
+    </div>
+    <!--右側エリア[[END]-->     
+
+</div>
+ <!--全エリア[END]-->
+
+</x-app-layout>
+
